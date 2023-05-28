8e0154f6882fc3cf747fd845ef41caf3c6c3f7bc
nikitach507 <chuprinnikita8@gmail.com>
2023-05-28 17:20:19 +0200

    Create a separate class to delete the note

---
+++ note_deletion.py
@@ -0,0 +1,18 @@
+from database.notes_database_action import NotesDatabaseAction
+from lib_imports import *
+
+
+class NoteDeletion:
+    @staticmethod
+    def delete_note_button(current_id: int):
+        """
+        Deletes a note from the database and updates the note display interface.
+
+        Args:
+            current_id (int): The ID of the note to be deleted.
+        """
+        answer = messagebox.askokcancel(
+            "Delete Note", "Are you sure you want to delete the note?"
+        )
+        if answer:
+            NotesDatabaseAction.delete_note("notes_info", note_id=current_id)
