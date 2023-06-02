ed5f25417ce5cbe0c7c3950c829a59d33ec7ef45
nikitach507 <chuprinnikita8@gmail.com>
2023-06-02 15:04:19 +0200

    Environment variables added

--- database/config.py
+++ database/config.py
@@ -1,4 +1,11 @@
-HOSTNAME = "127.0.0.1"
-DATABASE = "notes"
-USERNAME = "root"
-PASSWORD = "rootroot"
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+HOSTNAME = os.getenv("HOSTNAME")
+DATABASE = os.getenv("DATABASE")
+USERNAME = os.getenv("USERNAME")
+PASSWORD = os.getenv("PASSWORD")
+
+
